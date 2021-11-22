72cc6651bf78bb06e5895e987d7c90c6c167f81c
ZaisHandika <zaishandika@yahoo.com>
2021-11-22 10:02:57 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// Pages
+import LandingPage from "./pages/LandingPage";
+
+// Components
+import Header from "./components/Header";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
